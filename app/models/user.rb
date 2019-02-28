31c430df298dd5aca6815c2e17d6d65f35bea6ea
ioqren <corriestoddard@gmail.com>
2019-02-27 16:54:44 -0800

    Prepare for initial Heroku push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: api_users
+# Table name: users
 #
 #  id              :bigint(8)        not null, primary key
 #  username        :string           not null
@@ -9,6 +9,8 @@
 #  session_token   :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
+#  first_name      :string
+#  last_name       :string
 #
 
 class User < ApplicationRecord
--- db/migrate/20190227191148_add_first_name_to_users.rb
+++ db/migrate/20190227191148_add_first_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddFirstNameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :api_users, :first_name, :string
-    add_column :api_users, :last_name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
   end
 end
