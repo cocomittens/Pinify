4d80d65fb335abb0ad467e10f07a250a055a1602
ioqren <corriestoddard@gmail.com>
2019-03-08 12:07:37 -0800

    Change stuff

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 class Splash extends React.Component {
     componentDidMount() {
         this.props.fetchPins(1);
+        this.props.fetchPins(2);
+        this.props.fetchPins(3);
         this.props.fetchBoards("mittens");
     }
 
