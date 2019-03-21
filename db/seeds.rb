dbd8ad3ed03039d7a1c54f8bc7f705d69f02ba52
ioqren <corriestoddard@gmail.com>
2019-03-21 12:13:20 -0700

    Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([{username: 'mittens', email: 'mittens@mittens.com', password: '123456', first_name: 'Mittens', last_name: 'Mittens'}]);
-Board.create([{id: 1, author_id: 'mittens', title: 'cats'}])
+User.create([{username: 'mittens', email: 'mittens@mittens.com', password: '123456', first_name: 'Mittens', last_name: 'Mittens'}])
+Board.create([{id: 1, author_id: 1, title: 'cats'}])
 Pin.create([{id: 1, author_id: 'mittens', board_id: 1, photoUrl: ('/Users/cgripens/Desktop/AppAcademy/pinify/app/assets/images/cat1.png')}])
